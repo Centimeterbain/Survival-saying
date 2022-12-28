a2ca786f011dc60da072a17481bb51864362651d
Centimeterbain <80384282+Centimeterbain@users.noreply.github.com>
2022-12-29 03:34:34 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,9 +3,11 @@ import * as smoothScrollbar from "https://cdn.skypack.dev/smooth-scrollbar@8.8.1
 function showalert(){
     alert("使用電腦進入網頁會有比較好的瀏覽體驗喔~");
   }
-
+//npm.greensock.com/:_authToken=70b5652d-1560-4225-9117-6a8054381efd
+@gsap:registry=https://npm.greensock.com
 npm install ./gsap-bonus.tgz
 npm install gsap@npm:@gsap/member
+
 
 
 const tl = gsap.timeline({
